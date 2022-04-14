1a5eeb77325911fb0b10307a4ad239bdb3d0242b
ChristianSteinmann <christian.jhardy.steinmann@gmail.com>
2022-04-14 16:25:40 +0200

    finalized firebase

--- src/routes/sign-in/sign-in.component.jsx
+++ src/routes/sign-in/sign-in.component.jsx
@@ -1,18 +1,21 @@
-import React from 'react'
-import { signInWithGooglePopup, createUserDocumentFromAuth } from '../../utils/firebase/firebase.utils.js'
+import React from "react";
+import {
+  signInWithGooglePopup,
+  createUserDocumentFromAuth,
+} from "../../utils/firebase/firebase.utils.js";
 
 const SignIn = () => {
-    const logGoogleUser = async () => {
-        const {user} = await signInWithGooglePopup();
-        console.log(user);
-        createUserDocumentFromAuth(user);
-    }
+  const logGoogleUser = async () => {
+    const { user } = await signInWithGooglePopup();
+    console.log(user);
+    const userDocRef = await createUserDocumentFromAuth(user);
+  };
   return (
     <div>
-        <h1>Sign in Page</h1>
-        <button onClick={logGoogleUser}>Sign in</button>
+      <h1>Sign in Page</h1>
+      <button onClick={logGoogleUser}>Sign in</button>
     </div>
-  )
-}
+  );
+};
 
-export default SignIn
+export default SignIn;
