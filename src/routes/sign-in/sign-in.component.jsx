b61b70764be7cb499ae9ae62546b9eaf9be560f1
ChristianSteinmann <christian.jhardy.steinmann@gmail.com>
2022-04-14 16:39:00 +0200

    added redirect auth

--- src/routes/sign-in/sign-in.component.jsx
+++ src/routes/sign-in/sign-in.component.jsx
@@ -1,19 +1,36 @@
 import React from "react";
+import { useEffect } from "react";
+
+import { getRedirectResult } from "firebase/auth";
+
 import {
+  auth,
   signInWithGooglePopup,
+  signInWithGoogleRedirect,
   createUserDocumentFromAuth,
 } from "../../utils/firebase/firebase.utils.js";
 
 const SignIn = () => {
+  useEffect(async() => {
+    const response = await getRedirectResult(auth);
+    console.log(response);
+    if(response){
+      const userDocRef = await createUserDocumentFromAuth(response.user);
+    }
+  }, []);
+
+
   const logGoogleUser = async () => {
     const { user } = await signInWithGooglePopup();
     console.log(user);
     const userDocRef = await createUserDocumentFromAuth(user);
   };
+
   return (
     <div>
       <h1>Sign in Page</h1>
       <button onClick={logGoogleUser}>Sign in</button>
+      <button onClick={signInWithGoogleRedirect}>Sign in Redirect</button>
     </div>
   );
 };
