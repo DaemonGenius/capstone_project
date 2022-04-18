23a67e469239ad348ec579f219741cd48b2ec76c
ChristianSteinmann <christian.jhardy.steinmann@gmail.com>
2022-04-18 16:21:40 +0200

    Added fixes

--- src/components/category-item/category-item.component.jsx
+++ src/components/category-item/category-item.component.jsx
@@ -1,11 +1,15 @@
 import {BackgroundImage, Body, CategoryItemContainer} from "./category-item.styles";
 
 import React from "react";
+import { useNavigate } from "react-router-dom"
 
 const CategoryItem = ({ category }) => {
-    const {imageUrl, title} = category;
+    const {imageUrl, title, route } = category;
+    const navigate = useNavigate();
+    const onNavigate = () => navigate(route);
+
   return (
-    <CategoryItemContainer>
+    <CategoryItemContainer onClick={onNavigate}>
       <BackgroundImage
         style={{
           backgroundImage: `url(${imageUrl})`,
